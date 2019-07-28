0010d06f4840e1609d63d4a0de53f7f74445f37e
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-07-27 14:06:49 -1000

    resolves planet requests

--- js/app.js
+++ js/app.js
@@ -11,21 +11,45 @@ function requestResources() {
     );
     console.log(returnedObject);
     if (resourceType.value === "people") {
-      let h2 = document.createElement("h2");
-      h2.innerHTML = returnedObject.name;
-      contentContainer.appendChild(h2);
-      let pGender = document.createElement("p");
-      pGender.innerHTML = returnedObject.gender;
-      contentContainer.appendChild(pGender);
-      let pSpecies = document.createElement("p");
+      let name = document.createElement("h2");
+      name.innerHTML = returnedObject.name;
+      contentContainer.appendChild(name);
+      let gender = document.createElement("p");
+      gender.innerHTML = returnedObject.gender;
+      contentContainer.appendChild(gender);
+      let species = document.createElement("p");
       let speciesRequest = new XMLHttpRequest();
       speciesRequest.addEventListener("load", function() {
         speciesObject = JSON.parse(this.responseText);
-        pSpecies.innerHTML = speciesObject.name;
+        species.innerHTML = speciesObject.name;
       });
       speciesRequest.open("GET", returnedObject.species);
       speciesRequest.send();
-      contentContainer.appendChild(pSpecies);
+      contentContainer.appendChild(species);
+    }
+    if (resourceType.value === "planets") {
+      let name = document.createElement("h2");
+      name.innerHTML = returnedObject.name;
+      contentContainer.appendChild(name);
+      let terrain = document.createElement("p");
+      terrain.innerHTML = returnedObject.terrain;
+      contentContainer.appendChild(terrain);
+      let population = document.createElement("p");
+      population.innerHTML = returnedObject.population;
+      contentContainer.appendChild(population);
+      let list = document.createElement("ul");
+      contentContainer.appendChild(list);
+      for (i = 0; i < returnedObject.films.length; i++) {
+        let listItem = document.createElement("li");
+        let filmRequest = new XMLHttpRequest();
+        filmRequest.addEventListener("load", function() {
+          filmObject = JSON.parse(this.responseText);
+          listItem.innerHTML = filmObject.title;
+        });
+        filmRequest.open("GET", returnedObject.films[i]);
+        filmRequest.send();
+        list.appendChild(listItem);
+      }
     }
   });
   request.open(
